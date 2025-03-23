11faeb4dbd1393302204212e36f438a86484c7c5
Ivan Ticona <doriansamt@gmail.com>
2025-03-22 21:08:32 -0400

    Updated

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,6 @@ app.use(express.json());
 // Rutas
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/conversations", require("./routes/conversations"));
-app.use('/api/upload', require('./routes/upload'));
 app.use('/api/profile', require('./routes/profile'));
 
 app.get("/", (req, res) => {
